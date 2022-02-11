a327c3891b03f1577bc57ff4781b627d47b2f7d1
CO18321 <43029351+CO18321@users.noreply.github.com>
2022-02-11 19:04:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("myImage").style.paddingLeft = "350px";
+    //make sidebar visible by giving width of 30% of viewport width
+    document.getElementById("mySidenav").style.width = "30vw";
+    document.getElementById("myImage").style.paddingLeft = "30vw";
 }
   
 function closeNav() {
+    // hide sidebar
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("myImage").style.paddingLeft = "0";
    
 }
 
+
+// Function to render image from google drive
 function getImage(){
-  // cors is disabled using an extention (CORS Unblock)
-  const image_id = '1cXHh0iabxLEWV4a4R61IZiTOANDE02g7';
-  const url =  'https://drive.google.com/uc?id='+ image_id;
-  fetch(url).then(res => {
+
+  // cors is disabled using an extention called "CORS Unblock"
+  // the fetched image file is downlaoded under 'sources'
+
+  const image_id = '1HaMRlJO1Zt_kg_2ZkwXfHb7GIbBM-62n';
+  const URL =  'https://drive.google.com/uc?id='+ image_id;
+  fetch(URL).then(res => {
     let image = document.getElementById('myImage');
     image.src = res.url;
+    console.log(res.url);
   });
+
 
 }
